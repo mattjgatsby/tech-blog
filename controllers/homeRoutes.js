3fa8ebfbe59eaf8eebf6534f65975a62c397342f
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-11-09 00:49:06 -0500

    finished routes on homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,4 +27,36 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/post/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+        },
+      ],
+    });
+    const posts = postData.get({ plain: true });
+    res.render("posts", {
+      ...posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+
+  res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
 module.exports = router;
