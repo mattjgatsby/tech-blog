268487319bb91197ac1dc5f668302ad5122ab3e9
Gatsby <111303029+mattjgatsby@users.noreply.github.com>
2022-12-01 19:52:36 -0500

    added bootstrap into main handlebars

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,23 +1,23 @@
 const commentFormHandler = async function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const commentText = document.querySelector('#comment-input')
+  const commentText = document.querySelector("#comment-input");
 
-    const response = await fetch('api/comments', {
-        method: 'POST',
-        body: JSON.stringify({
-            comment: commentText.value 
-        }),
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
-    if(response.ok) {
-        document.location.reload()
-    } else {
-        alert("Could not post comment")
-    }
+  const response = await fetch("api/comments", {
+    method: "POST",
+    body: JSON.stringify({
+      comment: commentText.value,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+  if (response.ok) {
+    document.location.reload();
+  } else {
+    alert("Could not post comment");
+  }
 };
 document
-    .querySelector('#comment-form')
-    .addEventListener('submit', commentFormHandler)
+  .querySelector("#comment-form")
+  .addEventListener("submit", commentFormHandler);
